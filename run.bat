89971a88e74493b440ab3648218a5e58b4c8d08e
Akash Verma <akashv1507@gmai.com>
2021-01-15 12:01:10 +0530

    requirement.txt and batcch file created

--- run.bat
+++ run.bat
@@ -1,7 +1,4 @@
-call venv\Scripts\activate.bat
-call python index_insMinwiseDemand.py
-call python index_insBlockwiseDemand.py
-call python index_insStagingBlockwiseDemand.py --want_updation True
-REM forecast adjustment
-call python index_adjustmentBeforeForecast.py
-call python index_dayAheadForecast.py
+call project_env\Scripts\activate.bat
+call python index_insFilteredScadaDemand.py
+call python index_insDayAheadForecast.py
+
